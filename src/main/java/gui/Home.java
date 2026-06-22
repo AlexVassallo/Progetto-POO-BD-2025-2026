8ed62f6dc1be4e3788cabcd2b2f473bace0efc51
alericcio44 <alessio.riccio4@studenti.unina.it>
2026-06-22 14:32:09 +0200

    aggiunta del codice gui

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Home {
     private static JFrame frame;
@@ -12,6 +13,26 @@ public class Home {
     JPasswordField inserirePasswordPasswordField;
     private JTextField textField1;
     JLabel label2;
+    public Home() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                /*
+                qua ci sara la parte del codice che verificherà i dati inseriti
+                 */
+                frame.setVisible(false);
+                PaginaPrincipale paginaPrincipale=new PaginaPrincipale();
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+@Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                Registrati registrati=new Registrati();
+            }
+        });
+    }
     public static void main(String[] args) {
          frame = new JFrame("login");
          frame.setContentPane(new Home().mainPanel);
